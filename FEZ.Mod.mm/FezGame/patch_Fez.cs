d7f5ffb1908ef6b9df68b7c396ef9e589ac00c60
AngelDE98 <nuda1998@gmail.com>
2015-03-13 22:14:04 +0100

    Change when FEZMod.Exit gets called. Either LiveSplit doesn't crash on my PC or it's fixed now.

--- FEZ.Mod.mm/FezGame/patch_Fez.cs
+++ FEZ.Mod.mm/FezGame/patch_Fez.cs
@@ -4,11 +4,8 @@ using FezGame.Mod;
 namespace FezGame {
     public class patch_Fez {
 
-        public void orig_Exit() {
-        }
-
-        public void Exit() {
-            orig_Exit();
+        public void OnExiting(Object sender, EventArgs args) {
+            //It's possible that FEZ doesn't contain this method and thus orig_OnExiting won't exist.
             FEZMod.Exit();
         }
 
