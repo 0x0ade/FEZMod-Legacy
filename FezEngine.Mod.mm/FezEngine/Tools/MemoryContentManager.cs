29292752e02442e636d6303248f981c83df771e2
AngelDE98 <nuda1998@gmail.com>
2015-08-13 19:34:55 +0200

    .Core: Enable loading external textures

--- FezEngine.Mod.mm/FezEngine/Tools/MemoryContentManager.cs
+++ FezEngine.Mod.mm/FezEngine/Tools/MemoryContentManager.cs
@@ -76,7 +76,7 @@ namespace FezEngine.Tools {
             for (int i = 0; i < count; i++) {
                 byte[] bytes = cachedAssets[assetNames_[i]];
                 string assetName = assetNames_[i].ToLower();
-                string filePath = ("Resources\\"+(assetName.ToLower())).Replace("\\", Path.DirectorySeparatorChar.ToString()).Replace("/", Path.DirectorySeparatorChar.ToString())+".xnb";
+                string filePath = Externalize(assetName) + ".xnb";
                 FileInfo file = new FileInfo(filePath);
                 if (!file.Exists) {
                     file.Directory.Create();
@@ -100,7 +100,7 @@ namespace FezEngine.Tools {
                 DumpAll();
             }
 
-            string filePath = ("Resources\\"+(assetName.ToLower())).Replace("\\", Path.DirectorySeparatorChar.ToString()).Replace("/", Path.DirectorySeparatorChar.ToString())+".xnb";
+            string filePath = Externalize(assetName) + ".xnb";
             FileInfo file = new FileInfo(filePath);
             if (file.Exists) {
                 FileStream fis = new FileStream(filePath, FileMode.Open);
@@ -147,9 +147,7 @@ namespace FezEngine.Tools {
                 return true;
             }
 
-            string filePath = ("Resources\\"+(assetName.ToLower())).Replace("\\", Path.DirectorySeparatorChar.ToString()).Replace("/", Path.DirectorySeparatorChar.ToString())+".xnb";
-            FileInfo file = new FileInfo(filePath);
-            if (file.Exists) {
+            if (File.Exists(Externalize(assetName) + ".xnb")) {
                 return true;
             }
 
@@ -176,6 +174,10 @@ namespace FezEngine.Tools {
                 orig_Preload();
             }
             FEZMod.Preload();
+        }
+
+        public static string Externalize(string assetName) {
+            return ("Resources\\" + (assetName.ToLower())).Replace("\\", Path.DirectorySeparatorChar.ToString()).Replace("/", Path.DirectorySeparatorChar.ToString());
         }
 
     }
---
+++ FezEngine.Mod.mm/FezEngine/Tools/SharedContentManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+
+namespace FezEngine.Tools {
+    public class SharedContentManager {
+
+        private class CommonContentManager {
+
+            private T orig_ReadAsset<T>(string assetName) {
+                return default(T);
+            }
+
+            private T ReadAsset<T>(string assetName) where T : class {
+                if (typeof(T) == typeof(Texture2D)) {
+                    string imagePath = MemoryContentManager.Externalize(assetName);
+                    string imageExtension = null;
+
+                    if (File.Exists(imagePath + ".png")) {
+                        imageExtension = ".png";
+                    } else if (File.Exists(imagePath + ".jpg")) {
+                        imageExtension = ".jpg";
+                    } else if (File.Exists(imagePath + ".jpeg")) {
+                        imageExtension = ".jpeg";
+                    } else if (File.Exists(imagePath + ".gif")) {
+                        imageExtension = ".gif";
+                    }
+
+                    if (imageExtension != null) {
+                        using (FileStream fs = new FileStream(imagePath + imageExtension, FileMode.Open)) {
+                            return Texture2D.FromStream(ServiceHelper.Game.GraphicsDevice, fs) as T;
+                        }
+                    }
+                }
+
+                return orig_ReadAsset<T>(assetName);
+            }
+
+        }
+
+    }
+}
+
