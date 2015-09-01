f64f964a2882bdabe593304fbe80b5d251c12968
AngelDE98 <nuda1998@gmail.com>
2015-09-01 22:17:11 +0200

    Try replacing the version number in devbuilds

--- build_main.sh
+++ build_main.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd FEZ.Mod.mm
+perl -0777 -pi -e 's/public static string Version = ".*";/public static string Version = "dev-'${BUILD_NUMBER}'";/gm' ./FEZ.Mod.mm/FezGame/Mod/FEZMod.cs
 xbuild
 cd ..
