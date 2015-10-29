94ce56b4f23050396e2f39f4d16db12a5c22ab38
Gyoo <gyozell@gmail.com>
2015-10-29 14:08:05 +0100

    Added several controller codes

--- FEZ.Speedrun.mm/FezGame/Speedrun/BOT/ControllerCodeHelper.cs
+++ FEZ.Speedrun.mm/FezGame/Speedrun/BOT/ControllerCodeHelper.cs
@@ -11,23 +11,65 @@ using FezEngine.Tools;
 using System.Collections.Generic;
 
 namespace FezGame.Speedrun.BOT {
-    public static class ControllerCodeHelper {
-        
-        public static KeySequence MonoclePainting = new KeySequence()
-            /*l r*///l: left; r: right; 0: not pressed; 1: pressed; 2: released;
-            /*0 1*/.Add(CodeInput.SpinRight)
-            /*0 2*/.AddFrame()
-            /*0 1*/.AddFrame(CodeInput.SpinRight)
-            /*1 2*/.AddFrame(CodeInput.SpinLeft)
-            /*2 1*/.AddFrame(CodeInput.SpinRight)
-            /*1 2*/.AddFrame(CodeInput.SpinLeft)
-            /*2 0*/.AddFrame()
-            /*1 0*/.AddFrame(CodeInput.SpinLeft)
-            /*2 0*/.AddFrame()
-            /*1 0*/.AddFrame(CodeInput.SpinLeft)
-            /*2 1*/.AddFrame(CodeInput.SpinRight);
-            /*0 2*/
-            /*0 0*/
-        
-    }
+	public static class ControllerCodeHelper {
+
+		public static KeySequence MonoclePainting = new KeySequence ()
+			/*l r*///l: left; r: right; 0: not pressed; 1: pressed; 2: released;
+			/*0 1*/.Add(CodeInput.SpinRight)
+			/*0 2*/.AddFrame()
+			/*0 1*/.AddFrame(CodeInput.SpinRight)
+			/*1 2*/.AddFrame(CodeInput.SpinLeft)
+			/*2 1*/.AddFrame(CodeInput.SpinRight)
+			/*1 2*/.AddFrame(CodeInput.SpinLeft)
+			/*2 0*/.AddFrame()
+			/*1 0*/.AddFrame(CodeInput.SpinLeft)
+			/*2 0*/.AddFrame()
+			/*1 0*/.AddFrame(CodeInput.SpinLeft)
+			/*2 1*/.AddFrame(CodeInput.SpinRight);
+		/*0 2*/
+		/*0 0*/
+
+		public static KeySequence PipeRoom = new KeySequence ()
+			.Add (CodeInput.Up)
+			.AddFrame (CodeInput.SpinRight)
+			.AddFrame (CodeInput.Left)
+			.AddFrame (CodeInput.Jump)
+			.AddFrame (CodeInput.Right)
+			.AddFrame (CodeInput.SpinLeft)
+			.AddFrame (CodeInput.Down)
+			.AddFrame (CodeInput.SpinRight);
+
+		public static KeySequence WaterfallDoor = new KeySequence ()
+			.Add (CodeInput.Left)
+			.AddFrame (CodeInput.SpinLeft)
+			.AddFrame (CodeInput.Left)
+			.AddFrame (CodeInput.Right)
+			.AddFrame (CodeInput.SpinRight)
+			.AddFrame (CodeInput.Down)
+			.AddFrame (CodeInput.Up)
+			.AddFrame (CodeInput.SpinLeft);
+
+		public static KeySequence Waterfall = new KeySequence ()
+			.Add (CodeInput.Left)
+			.AddFrame (CodeInput.SpinLeft)
+			.AddFrame (CodeInput.Right)
+			.AddFrame (CodeInput.SpinRight)
+			.AddFrame (CodeInput.Up)
+			.AddFrame (CodeInput.Jump)
+			.AddFrame (CodeInput.Down)
+			.AddFrame (CodeInput.SpinRight)
+			.AddFrame ()
+			.AddFrame (CodeInput.SpinRight);
+
+		public static KeySequence InfiniteFall = new KeySequence ()
+			.Add (CodeInput.Right)
+			.AddFrame (CodeInput.SpinLeft)
+			.AddFrame (CodeInput.Right)
+			.AddFrame (CodeInput.Jump)
+			.AddFrame (CodeInput.SpinRight)
+			.AddFrame (CodeInput.Down)
+			.AddFrame (CodeInput.Jump)
+			.AddFrame (CodeInput.Up);
+
+	}
 }
