b3cdd3fb0e40dbbabf930cd83a372bf84a723734
AngelDE98 <nuda1998@gmail.com>
2015-11-29 18:49:02 +0100

    .Droid: Draw on-screen keys

--- FEZ.Mod.mm/FezGame/Droid/FezDroidComponent.cs
+++ FEZ.Mod.mm/FezGame/Droid/FezDroidComponent.cs
@@ -14,6 +14,7 @@ using Microsoft.Xna.Framework.Audio;
 using FezGame.Components;
 using FezGame.Mod;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace FezGame.Droid {
     public class FezDroidComponent : DrawableGameComponent {
@@ -39,6 +40,8 @@ namespace FezGame.Droid {
         [ServiceDependency]
         public ICollisionManager CollisionManager { private get; set; }
         
+        public SpriteBatch SpriteBatch;
+        
         private SoundEffect swooshLeft;
         private SoundEffect swooshRight;
         private SoundEffect slowSwooshLeft;
@@ -50,6 +53,32 @@ namespace FezGame.Droid {
         public Vector2 Drag = Vector2.Zero;
         public bool Dragging = true;
         private int dragTouchId = -1;
+        
+        private CodeInputAll[] buttonMapping = {
+            CodeInputAll.CancelTalk,
+            CodeInputAll.Jump,
+            CodeInputAll.GrabThrow,
+            CodeInputAll.OpenInventory,
+            CodeInputAll.Start,
+            CodeInputAll.Back
+        };
+        private Vector2[] buttonPosition = {
+            new Vector2(-1f, -2f),
+            new Vector2(-2f, -1f),
+            new Vector2(-4f, -2f),
+            new Vector2(-2f, 2f),
+            new Vector2(-1f, -1f),
+            new Vector2(1f, -1f)
+        };
+        private Vector2[] buttonPre = {
+            new Vector2(1f, 1f),
+            new Vector2(1f, 1f),
+            new Vector2(1f, 1f),
+            new Vector2(1f, 0f),
+            new Vector2(0.5f, 1f),
+            new Vector2(0.5f, 1f)
+        };
+        private Texture2D[] buttonTexture = new Texture2D[6];
         
         public FezDroidComponent(Game game) 
             : base(game) {
@@ -66,14 +95,19 @@ namespace FezGame.Droid {
                 return FakeInputHelper.Updating ? tmpFreeLook : new Vector2(0f, 0f);
             };
             FakeInputHelper.set_FreeLook = delegate(Vector2 value) {
-                if (FakeInputHelper.Updating) {
-                }
                 tmpFreeLook = value;
             };
         }
         
         protected override void LoadContent() {
             base.LoadContent();
+            
+            buttonTexture[0] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/BBUTTON");
+            buttonTexture[1] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/ABUTTON");
+            buttonTexture[2] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/XBUTTON");
+            buttonTexture[3] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/YBUTTON");
+            buttonTexture[4] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/STARTBUTTON");
+            buttonTexture[5] = CMProvider.Global.Load<Texture2D>("Other Textures/Glyphs/BACKBUTTON");
             
             swooshLeft = CMProvider.Global.Load<SoundEffect>("Sounds/Ui/RotateLeft");
             swooshRight = CMProvider.Global.Load<SoundEffect>("Sounds/Ui/RotateRight");
@@ -114,6 +148,45 @@ namespace FezGame.Droid {
             }
             
         }
+        
+        public override void Draw(GameTime gameTime) {
+            if (!FEZMod.Preloaded || GraphicsDevice == null || SpriteBatch == null) {
+                if (GraphicsDevice != null) {
+                    SpriteBatch = new SpriteBatch(GraphicsDevice);
+                }
+                return;
+            }
+
+            Viewport viewport = GraphicsDevice.Viewport;
+            float viewScale = GraphicsDevice.GetViewScale();
+
+            GraphicsDevice.SetBlendingMode(BlendingMode.Alphablending);
+            SpriteBatch.BeginPoint();
+            
+            float buttonScale = 4f * viewScale;
+            float buttonWidth = 16f;
+            float buttonHeight = 16f;
+            
+            for (int i = 0; i < buttonMapping.Length; i++) {
+                Texture2D tex = buttonTexture[i];
+                Vector2 pos = buttonPosition[i];
+                Vector2 pre = buttonPre[i];
+                SpriteBatch.Draw(tex, 
+                    new Vector2(
+                        pre.X * viewport.Width + (pos.X - 1) * buttonWidth * buttonScale,
+                        pre.Y * viewport.Height + (pos.Y - 1) * buttonHeight * buttonScale
+                    ), null,
+                    Color.White,
+                    0.0f,
+                    Vector2.Zero,
+                    buttonScale,
+                    SpriteEffects.None,
+                    0.0f);
+            }
+            
+            SpriteBatch.End();
+        }
+        
         
         public bool HandleButtonAt(TouchLocation tl) {
             if (Intro.Instance != null && Intro.Instance.Enabled && Intro.Instance.Visible) {
